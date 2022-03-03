17af216616790ad1fbdf374329642140973531f0
Bryan Peraza <Bryan Peraza@DESKTOP-FLMNRN9>
2022-03-02 23:19:25 -0600

    Proveedor Service 1

---
+++ WBL/dbo/ParametroService.cs
@@ -0,0 +1,115 @@
+﻿using BD;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WBL.dbo
+{
+    public class ParametroService
+    {
+        private readonly IDataAccess sql;
+
+        public ParametroService(IDataAccess _sql)
+        {
+            sql = _sql;
+        }
+
+        #region MetodosCRUD
+
+        //Metodo Get
+        public async Task<IEnumerable<ParametroEntity>> Get()
+        {
+            try
+            {
+                var result = sql.QueryAsync<ParametroEntity>("dbo.ParametroObtener");
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //Metodo GetById
+        public async Task<ParametroEntity> GetById(ParametroEntity entity)
+        {
+            try
+            {
+                var result = sql.QueryFirstAsync<ParametroEntity>("dbo.ParametroObtener", new { entity.Id_Parametro });
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //Metodo Crear
+        public async Task<DBEntity> Create(ParametroEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("dbo.ParametroInsertar", new
+                { entity.Codigo
+                  ,entity.Descripcion
+                  ,entity.Valor
+                  ,entity.Estado
+                });
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //Metodo Actualizar
+        public async Task<DBEntity> Update(ParametroEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("dbo.ParametroActualizar", new
+                {
+                   entity.Id_Parametro
+                  ,entity.Codigo
+                  ,entity.Descripcion
+                  ,entity.Valor
+                  ,entity.Estado
+                });
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //Metodo Actualizar
+        public async Task<DBEntity> Delete(ParametroEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("dbo.ParametroEliminar", new
+                {
+                   entity.Id_Parametro
+                  
+                });
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        #endregion
+    }
+}
